9220c28f0e842cfa13256d2ed8a9fc6058650a01
iamdaichi <takanogi2468@gmail.com>
2023-04-10 16:15:50 +0100

    first commit

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <main className='about'>
       <div className='wrapper'>
         <h2>About this website</h2>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse commodi, eum quis similique animi odit cum pariatur. Ratione explicabo laborum obcaecati ut corporis repellat quas aut? Vel, ab eligendi maxime architecto mollitia quidem exercitationem atque aliquid, voluptas sint omnis amet? Exercitationem, ratione! Corrupti dolor itaque illum quia harum. Et laboriosam, expedita quasi ex iusto impedit adipisci odio dolorum explicabo corrupti rem, quaerat quo fugit dolores autem ea? Maiores enim expedita obcaecati laborum aliquid in molestiae iste reprehenderit possimus! Voluptates temporibus possimus officiis corrupti! Numquam odit inventore aliquid consequuntur accusamus repellat nesciunt animi laudantium? Officiis perspiciatis quidem atque. Magnam, quas amet!</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse commodi, eum quis similique animi odit cum pariatur. Ratione explicabo laborum obcaecati ut corporis repellat quas aut? Vel, ab eligendi maxime architecto mollitia quidem exercitationem atque aliquid, voluptas sint omnis amet? Exercitationem, ratione! Corrupti dolor itaque illum quia harum. Et laboriosam, expedita quasi ex iusto impedit adipisci odio dolorum explicabo corrupti rem, quaerat quo fugit dolores autem ea? Maiores enim expedita obcaecati laborum aliquid in molestiae iste reprehenderit possimus! Voluptates temporibus possimus officiis corrupti! Numquam odit inventore aliquid consequuntur accusamus repellat nesciunt animi laudantium? Officiis perspiciatis quidem atque. Magnam, quas amet!</p>
       </div>
     </main>
   )
